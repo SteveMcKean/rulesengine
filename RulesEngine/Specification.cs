bc9d8912966e6b6ec570266fe930edc948afc125
Steve McKean <steven.mckean@peraton.com>
2024-11-11 10:16:50 -0600

    updated specficiation.

--- RulesEngine/Specification.cs
+++ RulesEngine/Specification.cs
@@ -21,10 +21,100 @@ public abstract class Specification<T>
             .FirstOrDefault(specification => specification.IsSatisfiedBy(entity).IsSatisfied);
     }
     
+    public Specification<T> And(Specification<T> specification)
+    {
+        return new AndSpecification<T>(this, specification);
+    }
+    
+    public Specification<T> Or(Specification<T> specification)
+    {
+        return new OrSpecification<T>(this, specification);
+    }
+    
+    public Specification<T> Not()
+    {
+        return new NotExpression<T>(this);
+    }
+    
     public abstract Expression<Func<T, bool>> ToExpression();
     
     public virtual string GetFailureMessage(T entity)
     {
         return $"{Name} was not satisfied.";
+    }
+    
+    internal sealed class AndSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> left;
+        private readonly Specification<T> right;
+        
+        public AndSpecification(Specification<T> left, Specification<T> right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+        
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            var leftExpression = left.ToExpression();
+            var rightExpression = right.ToExpression();
+            
+            var andExpression = Expression.AndAlso(leftExpression.Body, rightExpression.Body);
+            return Expression.Lambda<Func<T, bool>>(andExpression, leftExpression.Parameters.Single());
+        }
+        
+        public override string GetFailureMessage(T entity)
+        {
+            return $"{left.GetFailureMessage(entity)} and {right.GetFailureMessage(entity)}";
+        }
+    }
+    
+    internal sealed class OrSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> left;
+        private readonly Specification<T> right;
+        
+        public OrSpecification(Specification<T> left, Specification<T> right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+        
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            var leftExpression = left.ToExpression();
+            var rightExpression = right.ToExpression();
+            
+            var orExpression = Expression.OrElse(leftExpression.Body, rightExpression.Body);
+            return Expression.Lambda<Func<T, bool>>(orExpression, leftExpression.Parameters.Single());
+        }
+        
+        public override string GetFailureMessage(T entity)
+        {
+            return $"{left.GetFailureMessage(entity)} or {right.GetFailureMessage(entity)}";
+        }
+    }
+    
+    internal sealed class NotExpression<T> : Specification<T>
+    {
+        private readonly Specification<T> specification;
+        
+        public NotExpression(Specification<T> specification)
+        {
+            this.specification = specification;
+        }
+        
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            var expression = specification.ToExpression();
+            var notExpression = Expression.Not(expression.Body);
+            
+            return Expression.Lambda<Func<T, bool>>(notExpression, expression.Parameters.Single());
+        }
+        
+        public override string GetFailureMessage(T entity)
+        {
+            return $"Not {specification.GetFailureMessage(entity)}";
+        }
     }
 }
